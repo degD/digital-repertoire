4f3b0b289f626450e44e4a1164084a50e20a7e78
degD <108623049+degD@users.noreply.github.com>
2024-08-11 18:46:45 +0300

    make search working

--- main.js
+++ main.js
@@ -1,11 +1,14 @@
 
-const searchField = document.querySelector("#search-field");
-const searchContainer = document.querySelector(".search");
+const searchField = document.querySelector("#search-bar");
+const searchResults = document.querySelector(".search-results");
 
 fetch("songs.json")
     .then(file => file.json())
     .then(songs => searchField.addEventListener("input", evt => {
 
+        searchResults.innerHTML = "";
+
+        const potentialNames = [];
         const input = evt.target.value;
         console.log(`Searching "${input}"...`);
 
@@ -13,12 +16,26 @@ fetch("songs.json")
 
             const songName = s.name;
             const inputIndex = songName.indexOf(input);
+            const resultCount = searchResults.childElementCount;
 
-            if (inputIndex != -1) {
+            if (inputIndex != -1 && resultCount < 9) {
+
+                const resultDiv = document.createElement("div");
+                resultDiv.className = "result"; 
+                resultDiv.innerHTML = songName
+                if (resultCount % 2) {
+                    resultDiv["style"]["background-color"] = "rgb(201, 201, 201)";
+                } else {
+                    resultDiv["style"]["background-color"] = "rgb(221, 221, 221)";
+                }
+
+                searchResults.appendChild(resultDiv);
 
                 console.log(`"${input}" in "${songName}"`);
             }
         }
+
+
 
     }))
     .catch(error => console.log(error));
