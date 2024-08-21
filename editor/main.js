1877484255b21fc5fae9b8199159edd8080030b6
degD <108623049+degD@users.noreply.github.com>
2024-08-21 16:33:38 +0300

    add checkboxes to select lines with chords

--- editor/main.js
+++ editor/main.js
@@ -1,3 +1,15 @@
 
+document.querySelectorAll(".chord-checkbox").forEach(b => {
+
+    // A line is selected as a chord line
+    b.addEventListener("click", evt => {
+
+
+        if (b.parentNode["style"]["background"] == "")
+            b.parentNode["style"]["background"] = "lightblue";
+        else 
+            b.parentNode["style"]["background"] = "";
+    });
+});
 
 
